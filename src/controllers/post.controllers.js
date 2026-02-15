234ca7e731f06cf9850f6d880fd5256b193a0fb7
soumishjain <jainmashu13@gmail.com>
2026-02-15 15:04:47 +0530

    getpost controller created

--- src/controllers/post.controllers.js
+++ src/controllers/post.controllers.js
@@ -1,23 +1,121 @@
 const ImageKit = require('@imagekit/nodejs');
-const {toFile} = require('@imagekit/nodejs')
+const { toFile } = require('@imagekit/nodejs')
+const jwt = require('jsonwebtoken')
 
 const imagekit = new ImageKit({
-  privateKey: process.env.IMAGEKIT_PRIVATE_KEY, 
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
 });
 
 
 const postModel = require('../models/post.models')
 
-async function createPost (req , res) {
-    console.log(req.body , req.file)
+async function createPost(req, res) {
 
   const file = await imagekit.files.upload({
-  file: await toFile(Buffer.from(req.file.buffer), 'file'),
-  fileName: 'TestImage',
-});
-res.send(file)
+    file: await toFile(Buffer.from(req.file.buffer), 'file'),
+    fileName: 'TestImage',
+    folder : 'cohort-2-insta-clone/posts'
+  });
+
+  const token = req.cookies.token
+  console.log(token)
+
+  if(!token) {
+    return res.status(401).json({
+      message : "Unauthorized Access"
+    })
+  }
+  let decode = null;
+  try{
+    decode = jwt.verify(token,process.env.JWT_SECRET)
+  }catch(err){
+    return res.status(401).json({
+      message : "Unauthorized Access"
+    })
+  }
+  
+
+  const post = await postModel.create({
+    caption : req.body.caption,
+    imageUrl : file.url,
+    user : decode.id
+  })
+
+  res.status(201).json({
+    message : "post Successfully created",
+    post
+  })
+
 }
 
-module.exports={
-    createPost
+async function getPostsfromUser(req,res){
+  const token = req.cookies.token;
+  if(!token){
+    return res.status(409).json({
+      message : "Unauthorized Access"
+    })
+  }
+  let decode;
+  try{
+    decode = jwt.verify(token,process.env.JWT_SECRET)
+  }catch(err){
+    return res.status(409).json({
+      message : "Unauthorized Access"
+    })
+  }
+  const userId = decode.id
+  const posts = await postModel.find({user : userId})
+
+  res.status(200).json({
+    message : "posts fetched successfully",
+    posts
+  })
+}
+
+async function getPostsDetail(req,res){
+  const token = req.cookies.token;
+  if(!token){
+    return res.status(409).json({
+      message : "Unauthorized Access"
+    })
+  }
+  let decode;
+  try{
+    decode = jwt.verify(token,process.env.JWT_SECRET)
+  }catch(err){
+    return res.status(409).json({
+      messaege : "Unauthorized Access"
+    })
+  }
+
+  const userId = decode.id
+  const postId = req.params.postId
+
+  const post = await postModel.findById(postId)
+
+  if(!post){
+    return res.status(404).json({
+      message : "not found"
+    })
+  }
+
+  const isUserValid = userId === post.user.toString()
+
+  if(!isUserValid) {
+    return res.status(403).json({
+      message : "forbidden content"
+    })
+  }
+
+  res.status(200).json({
+    message : "req fetched succesfully",
+    post
+  })
+
+}
+
+module.exports = {
+  createPost,
+  getPostsfromUser,
+  getPostsDetail
 }
--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -6,5 +6,7 @@ const upload = multer({storage : multer.memoryStorage()})
 
 
 postRouter.post('/', upload.single("imageUrl") , postController.createPost)
+postRouter.get('/',postController.getPostsfromUser)
+postRouter.get('/detail/:postId',postController.getPostsDetail)
 
 module.exports=postRouter
